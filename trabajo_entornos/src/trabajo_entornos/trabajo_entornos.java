f6625a256329d4465b6f5260bfe0b28b992a3afe
f009979n <f009979n@MSCDIT00N.home>
2020-11-11 21:11:02 +0100

    trabajo entornos

---
+++ trabajo_entornos/src/trabajo_entornos/trabajo_entornos.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package trabajo_entornos;
+
+/**
+ * @author f009979n
+ *
+ */
+public class trabajo_entornos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
